6f158afe1e6f032c7d0c480bc61bbf41bf85cc28
Curt Cox <curtcox@gmail.com>
2019-10-20 12:38:44 -0500

    Update Java version which forced JUnit downgrade.

--- src/test/java/edu/drexel/bunch4/AppTest.java
+++ src/test/java/edu/drexel/bunch4/AppTest.java
@@ -1,7 +1,7 @@
 package edu.drexel.bunch4;
 
-import static junit.framework.Assert.*;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
 
